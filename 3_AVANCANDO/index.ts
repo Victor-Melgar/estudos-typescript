d837af9b82480a96dfaa4505aad65da49806173f
Victor-Melgar <gabrielmelgar.digital@gmail.com>
2024-10-02 20:18:37 -0300

    estudos-typescript

---
+++ 3_AVANCANDO/index.ts
@@ -0,0 +1,23 @@
+// 1 - arrays
+let numbers: number[] = [1, 2, 3];
+
+numbers.push(5);
+
+console.log(numbers[2])
+
+// numbers = "teste"
+
+const nomes: string[] = ["Gabriel", "Victor"]
+
+// nomes.push(4)
+
+// 2 - outra sintaxe array
+const nums: Array<number> = [100, 200];
+
+nums.push(300);
+
+console.log(nums);
+
+// nums.push("teste")
+
+console.log(nums[5]);
